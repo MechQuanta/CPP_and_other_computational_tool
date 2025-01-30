aca4b9cc42f81b86a082e29352c8ad1ccce22349
MechQuanta <sk0065756@gmail.com>
2025-01-30 21:22:16 +0600

    not efficient

---
+++ stuff_cpp/num_analysis/fixed_point_iteration3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+float func(float x);
+int main(int argc,char* argv[]){
+  float p0,p,tol;
+  int i,N0;
+  i=0;
+  ofstream filee("fixedpointiteration_work_but_not_efficient.out");
+  cout<<"initial guess: ";
+  cin>>p0;
+  p = p0;
+  cout<<"How many Iteration: ";
+  cin>>N0;
+  cout<<"Tolerance: ";
+  cin>>tol;
+  filee<<"i"<<"\t"<<"p"<<"\t"<<"|p-p0|"<<endl;
+  filee<<i<<"\t"<<p0<<"\t"<<abs(p0-p)<<endl;
+  while(i<=N0){
+    p = func(p0);
+    filee<<i<<"\t"<<p0<<"\t"<<abs(p0-p)<<endl;
+    if (abs(p-p0)<tol){
+      cout<<"This is output solution: "<<p;
+    }
+    i = i + 1;
+    p0 = p;
+  }
+  cout<<"Ultimate output: "<<p<<endl;
+  return 0;
+}
+float func(float x){
+  return (0.5*sqrt(10-x*x*x));
+}
