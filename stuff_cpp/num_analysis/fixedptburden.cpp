07e4d0609133800a93e8600e7e01858d75ac3825
MechQuanta <sk0065756@gmail.com>
2025-02-08 16:48:30 +0600

    tryin somethin new

---
+++ stuff_cpp/num_analysis/fixedptburden.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+float func(float x){
+  return (x*x-2);
+}
+int main(int argc, char const *argv[]) {
+  float a,b,p,tol,p0;
+  int N0,i;
+  cout<<"Give the left boundary(a): ";
+  cin>>a;
+  cout<<"Give the right boundary(b): ";
+  cin>>b;
+  cout<<"give the in between value(p): ";
+  cin>>p;
+  cout<<"Total number: ";
+  cin>>N0;
+  cout<<"give tolerance: ";
+  cin>>tol;
+  ofstream filew("burdenp56.dat");
+  filew<<"iteration"<<"/t"<<"p=g(p)"<<"/t"<<"absolute err";
+  //p0 = func(p);
+  i=0;
+  while(true){
+    p0 = func(p);
+    if(a<=p<=b){
+      if(abs(p-p0)<tol){
+        filew<<i<<"/t"<<p0<<"/t"<<abs(p-p0);
+        break;
+      }
+    else{
+      filew<<i<<"/t"<<p0<<"/t"<<abs(p-p0);
+      p=p0;
+    }
+    }
+    i++;
+  }
+  return 0;
+}
